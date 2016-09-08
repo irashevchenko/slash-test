da33417f3b7ade6aa6fd1a6477641afe22239a77
Irishka <irishka0513@yandex.ru>
2016-09-08 18:01:26 +0300

    Add video on Vimeo

--- js/js.js
+++ js/js.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
-    var slider; var prevClick;
+  var slider; var prevClick;
   $('#menu').on("click","a",function(e){
    e.preventDefault();
    var id = $(this).attr('href');
    scroll_menu(id);
-      prevClick = e.target.hash;
-      console.log(prevClick);
+   prevClick = id;
  });
 
  $('#select-menu').on("change", function () {
    var id = $('#select-menu').val();
    scroll_menu(id);
+   prevClick = id;
  });
 
-    function initSlider() {
-        slider = $('.bxslider').bxSlider({
-            mode: 'vertical',
-            captions: true,
-            auto: true,
-            easing: 'easeOutBack',
-            speed: 1000,
-            autoHover: true,
-            controls: false,
-            slideWidth: 300
-        });
-    };
-    initSlider();
-
+  function initSlider() {
+      slider = $('.bxslider').bxSlider({
+          mode: 'vertical',
+          captions: true,
+          auto: true,
+          easing: 'easeOutBack',
+          speed: 1000,
+          autoHover: true,
+          controls: false,
+          slideWidth: 300
+      });
+  };
+  initSlider();
 
   $('.portfolio-img').mouseover(function(){
   	$(this).find('.invisible').addClass("visible");
   }).mouseout(function(){
   	$('.invisible').removeClass("visible");
   });
+
   $(document).on("click", '.visible', function(){
   	$(this).closest('.portfolio-img').find('img[data-fancybox-href]').click();
   });
+
   $('[data-fancybox-href]').fancybox({
   	openEffect	: 'elastic',
     closeEffect	: 'elastic',
@@ -46,27 +47,23 @@ $(document).ready(function() {
     }
   });
 
-    function scroll_menu(id) {
-        if (id == '#features') {
-            $('#features').slideDown();
-            slider.destroySlider();
-            initSlider();
-        }
-        else{
-            $('#features').slideUp();
-        }
-        var featuresTop = $('#features').height();
-        var headerTop= $('#header').height();
-        var top = $(id).offset().top;
-
-        console.log(prevClick);
-        if ((id == '#portfolio') && (prevClick =='#features')|| (id =='#contact')&& (prevClick =='#features')) {
-            var featuresTop = $('#features').height();
-            $('body,html').animate({scrollTop: top-headerTop-featuresTop}, 1500);
-        }
-        else{
-            $('body,html').animate({scrollTop: top-headerTop}, 1500);
-        }
+  function scroll_menu(id) {
+    if (id == '#features') {
+      $('#features').slideDown();
+      slider.destroySlider();
+      initSlider();
+    } else{
+      $('#features').slideUp();
     }
+    var featuresTop = $('#features').height();
+    var headerTop= $('#header').height();
+    var top = $(id).offset().top;
+    if ((id == '#portfolio') && (prevClick =='#features')|| (id =='#contact')&& (prevClick =='#features')) {
+      var featuresTop = $('#features').height();
+      $('body,html').animate({scrollTop: top-headerTop-featuresTop}, 1500);
+    } else{
+      $('body,html').animate({scrollTop: top-headerTop}, 1500);
+    }
+  }
 });
 
